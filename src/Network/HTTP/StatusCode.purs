3dd90274b92e6b29b29a5c052d9d953c21232cfe
Robert Klotzner <robert.klotzner@gmx.at>
2016-06-27 17:31:38 +0200

    Added some Generic instances

--- src/Network/HTTP/StatusCode.purs
+++ src/Network/HTTP/StatusCode.purs
@@ -1,8 +1,11 @@
 module Network.HTTP.StatusCode where
 
 import Prelude
+import Data.Generic (class Generic)
 
 newtype StatusCode = StatusCode Int
+
+derive instance genericStatusCode :: Generic StatusCode
 
 instance eqStatusCode :: Eq StatusCode where
   eq (StatusCode x) (StatusCode y) = x == y
