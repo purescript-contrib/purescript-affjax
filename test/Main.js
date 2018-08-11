20bc9b42bcbce3c1d975d6f766999ed411af1e29
Gary Burgess <gary.burgess@gmail.com>
2018-08-11 16:08:34 +0100

    Start and stop test server as part of running the tests

--- test/Main.js
+++ test/Main.js
@@ -6,3 +6,59 @@ exports.logAny = function (a) {
     return {};
   };
 };
+
+exports.startServer = function (errback, callback) {
+  var express = require('express');
+  var app = express();
+  var bodyParser = require('body-parser');
+
+  // Always make req.body available as a String
+  app.use(bodyParser.text(function() { return true; }));
+
+  app.use(express.static(__dirname));
+
+  app.get('/', function (req, res) {
+    res.send('<html><script src="tmp/test.js"></script></html>');
+  });
+
+  app.get('/arrayview', function(req, res) {
+    res.send('TODO');
+  });
+
+  app.get('/not-json', function(req, res) {
+    res.header({'content-type': 'text/plain'});
+    res.send('This is not JSON');
+  });
+
+  app.all('/mirror', function(req, res) {
+    res.json({
+      headers: req.headers,
+      body: req.body,
+      query: req.query,
+      method: req.method,
+    });
+  });
+
+  var server = app.listen(function () {
+    callback({ port: server.address().port, server: server });
+  });
+  server.on('error', function (error) {
+    errback(error);
+  });
+
+  return function (cancelError, onCancelerError, onCancelerSuccess) {
+    onCancelerSuccess();
+  };
+};
+
+exports.stopServer = function (server) {
+  return function (errback, callback) {
+    server.close(function (err) {
+      if (err) errback(err);
+      else callback();
+    });
+    return function (cancelError, onCancelerError, onCancelerSuccess) {
+      onCancelerSuccess();
+    };
+  };
+};
--- test/Main.purs
+++ test/Main.purs
@@ -12,7 +12,8 @@ import Data.Either (Either(..), either)
 import Data.Maybe (Maybe(..))
 import Data.Time.Duration (Milliseconds(..))
 import Effect (Effect)
-import Effect.Aff (Aff, forkAff, attempt, runAff, killFiber)
+import Effect.Aff (Aff, attempt, finally, forkAff, killFiber, runAff)
+import Effect.Aff.Compat (EffectFnAff, fromEffectFnAff)
 import Effect.Class (liftEffect)
 import Effect.Class.Console as A
 import Effect.Console (log, logShow)
@@ -20,6 +21,10 @@ import Effect.Exception (error, throwException)
 import Foreign.Object as FO
 
 foreign import logAny :: forall a. a -> Effect Unit
+
+foreign import data Server :: Type
+foreign import startServer :: EffectFnAff { server :: Server, port :: Int }
+foreign import stopServer :: Server -> EffectFnAff Unit
 
 logAny' :: forall a. a -> Aff Unit
 logAny' = liftEffect <<< logAny
@@ -50,49 +55,53 @@ main = void $ runAff (either (\e -> logShow e *> throwException e) (const $ log 
   let ok200 = StatusCode 200
   let notFound404 = StatusCode 404
 
-  -- A complete URL is necessary for tests to work on Node.js
-  let prefix = append "http://localhost:3838"
-  let mirror = prefix "/mirror"
-  let doesNotExist = prefix "/does-not-exist"
-  let notJson = prefix "/not-json"
   let retryPolicy = AX.defaultRetryPolicy { timeout = Just (Milliseconds 500.0), shouldRetryWithStatusCode = \_ -> true }
 
-  A.log "GET /does-not-exist: should be 404 Not found after retries"
-  (attempt $ AX.retry retryPolicy (AX.request ResponseFormat.ignore) $ AX.defaultRequest { url = doesNotExist }) >>= assertRight >>= \res -> do
-    assertEq notFound404 res.status
+  { server, port } ← fromEffectFnAff startServer
+  finally (fromEffectFnAff (stopServer server)) do
+    A.log ("Test server running on port " <> show port)
 
-  A.log "GET /mirror: should be 200 OK"
-  (attempt $ AX.request ResponseFormat.ignore $ AX.defaultRequest { url = mirror }) >>= assertRight >>= \res -> do
-    assertEq ok200 res.status
+    let prefix = append ("http://localhost:" <> show port)
+    let mirror = prefix "/mirror"
+    let doesNotExist = prefix "/does-not-exist"
+    let notJson = prefix "/not-json"
 
-  A.log "GET /does-not-exist: should be 404 Not found"
-  (attempt $ AX.request ResponseFormat.ignore $ AX.defaultRequest { url = doesNotExist }) >>= assertRight >>= \res -> do
-    assertEq notFound404 res.status
+    A.log "GET /does-not-exist: should be 404 Not found after retries"
+    (attempt $ AX.retry retryPolicy (AX.request ResponseFormat.ignore) $ AX.defaultRequest { url = doesNotExist }) >>= assertRight >>= \res -> do
+      assertEq notFound404 res.status
 
-  A.log "GET /not-json: invalid JSON with Foreign response should throw an error"
-  void $ assertLeft =<< attempt (AX.get ResponseFormat.json doesNotExist)
+    A.log "GET /mirror: should be 200 OK"
+    (attempt $ AX.request ResponseFormat.ignore $ AX.defaultRequest { url = mirror }) >>= assertRight >>= \res -> do
+      assertEq ok200 res.status
 
-  A.log "GET /not-json: invalid JSON with String response should be ok"
-  (attempt $ AX.get ResponseFormat.string notJson) >>= assertRight >>= \res -> do
-    assertEq ok200 res.status
+    A.log "GET /does-not-exist: should be 404 Not found"
+    (attempt $ AX.request ResponseFormat.ignore $ AX.defaultRequest { url = doesNotExist }) >>= assertRight >>= \res -> do
+      assertEq notFound404 res.status
 
-  A.log "POST /mirror: should use the POST method"
-  (attempt $ AX.post ResponseFormat.json mirror (RequestBody.string "test")) >>= assertRight >>= \res -> do
-    assertEq ok200 res.status
-    assertEq (Just "POST") (J.toString =<< FO.lookup "method" =<< J.toObject res.response)
+    A.log "GET /not-json: invalid JSON with Foreign response should throw an error"
+    void $ assertLeft =<< attempt (AX.get ResponseFormat.json doesNotExist)
 
-  A.log "PUT with a request body"
-  let content = "the quick brown fox jumps over the lazy dog"
-  (attempt $ AX.put ResponseFormat.json mirror (RequestBody.string content)) >>= assertRight >>= \res -> do
-    assertEq ok200 res.status
-    assertEq (Just "PUT") (J.toString =<< FO.lookup "method" =<< J.toObject res.response)
-    assertEq (Just content) (J.toString =<< FO.lookup "body" =<< J.toObject res.response)
+    A.log "GET /not-json: invalid JSON with String response should be ok"
+    (attempt $ AX.get ResponseFormat.string notJson) >>= assertRight >>= \res -> do
+      assertEq ok200 res.status
 
-  A.log "Testing CORS, HTTPS"
-  (attempt $ AX.get ResponseFormat.json "https://cors-test.appspot.com/test") >>= assertRight >>= \res -> do
-    assertEq ok200 res.status
-    -- assertEq (Just "test=test") (lookupHeader "Set-Cookie" res.headers)
+    A.log "POST /mirror: should use the POST method"
+    (attempt $ AX.post ResponseFormat.json mirror (RequestBody.string "test")) >>= assertRight >>= \res -> do
+      assertEq ok200 res.status
+      assertEq (Just "POST") (J.toString =<< FO.lookup "method" =<< J.toObject res.response)
 
-  A.log "Testing cancellation"
-  forkAff (AX.post_ mirror (RequestBody.string "do it now")) >>= killFiber (error "Pull the cord!")
-  assertMsg "Should have been canceled" true
+    A.log "PUT with a request body"
+    let content = "the quick brown fox jumps over the lazy dog"
+    (attempt $ AX.put ResponseFormat.json mirror (RequestBody.string content)) >>= assertRight >>= \res -> do
+      assertEq ok200 res.status
+      assertEq (Just "PUT") (J.toString =<< FO.lookup "method" =<< J.toObject res.response)
+      assertEq (Just content) (J.toString =<< FO.lookup "body" =<< J.toObject res.response)
+
+    A.log "Testing CORS, HTTPS"
+    (attempt $ AX.get ResponseFormat.json "https://cors-test.appspot.com/test") >>= assertRight >>= \res -> do
+      assertEq ok200 res.status
+      -- assertEq (Just "test=test") (lookupHeader "Set-Cookie" res.headers)
+
+    A.log "Testing cancellation"
+    forkAff (AX.post_ mirror (RequestBody.string "do it now")) >>= killFiber (error "Pull the cord!")
+    assertMsg "Should have been canceled" true
