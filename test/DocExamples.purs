d710d92a46f8afb165214be07d452fd586c78c0d
Gary Burgess <gary.burgess@gmail.com>
2019-11-02 14:12:01 +0000

    Update for fallible `FormURLEncoded`, don't use `Aff` error channel anymore.
    Also cut down on the helper functions by just using `Maybe RequestBody.RequestBody`, and remove the retrying stuff that I doubt anybody uses.

--- test/DocExamples.purs
+++ test/DocExamples.purs
@@ -8,25 +8,26 @@ import Affjax.ResponseFormat as ResponseFormat
 import Data.Argonaut.Core as J
 import Data.Either (Either(..))
 import Data.HTTP.Method (Method(..))
+import Data.Maybe (Maybe(..))
 import Effect (Effect)
 import Effect.Aff (launchAff)
 import Effect.Class.Console (log)
 
 main :: Effect Unit
 main = void $ launchAff $ do
-  res <- AX.request (AX.defaultRequest { url = "/api", method = Left GET, responseFormat = ResponseFormat.json })
-  case res.body of
-    Left err -> log $ "GET /api response failed to decode: " <> AX.printResponseFormatError err
-    Right json -> log $ "GET /api response: " <> J.stringify json
+  result <- AX.request (AX.defaultRequest { url = "/api", method = Left GET, responseFormat = ResponseFormat.json })
+  case result of
+    Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "GET /api response: " <> J.stringify response.body
 
 main' :: Effect Unit
 main' = void $ launchAff $ do
-  res1 <- AX.get ResponseFormat.json "/api"
-  case res1.body of
-    Left err -> log $ "GET /api response failed to decode: " <> AX.printResponseFormatError err
-    Right json -> log $ "GET /api response: " <> J.stringify json
+  result1 <- AX.get ResponseFormat.json "/api"
+  case result1 of
+    Left err -> log $ "GET /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "GET /api response: " <> J.stringify response.body
 
-  res2 <- AX.post ResponseFormat.json "/api" (RequestBody.json (J.fromString "test"))
-  case res2.body of
-    Left err -> log $ "POST /api response failed to decode: " <> AX.printResponseFormatError err
-    Right json -> log $ "POST /api response: " <> J.stringify json
+  result2 <- AX.post ResponseFormat.json "/api" (Just (RequestBody.json (J.fromString "test")))
+  case result2 of
+    Left err -> log $ "POST /api response failed to decode: " <> AX.printError err
+    Right response -> log $ "POST /api response: " <> J.stringify response.body
